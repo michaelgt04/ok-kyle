d5c8395b3900fef51aacac5f5b26bc71ba38b037
Amanda Beiner <amandabeiner@Amandas-MacBook-Air.local>
2017-01-16 18:52:47 -0500

    User authentication and tests

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,7 @@
+class AuthController < ApplicationController
+  def failure
+    flash[:notice] = "Authentication failed"
+    redirect_to root_path
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
+  root 'users#new'
+  get 'auth/:provider/callback' => 'sessions#create'
+  get '/signout' => 'sessions#destroy', :as => :signout
+  get 'auth/failure'
 end
