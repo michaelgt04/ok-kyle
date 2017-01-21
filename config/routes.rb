7e1df1103aeee2e222c4572526bc51acb0ee33f7
Mike Thomson <michaelgt04@gmail.com>
2017-01-21 14:38:26 -0500

    Added swipe right functionality with API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :kyles, only: [:index]
-      resources :matches, only: [:index]
+      resources :matches, only: [:index, :create]
     end
   end
 end
