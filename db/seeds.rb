1ca9938e3037224c05fddbcaad8c8da9f702f4df
Mike Thomson <michaelgt04@gmail.com>
2017-01-21 16:37:34 -0500

    Added foundation and carousel for the pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 
 Kyle.create(name: "Sporty Kyle", image_url: "http://res.cloudinary.com/duor0bzmo/image/upload/v1485032696/img_0492_720_qfectr.jpg", description: "You like sports and so does he.")
 Kyle.create(name: "Mediation Kyle", image_url: "http://res.cloudinary.com/duor0bzmo/image/upload/v1485032575/imageedit_3_2423010833_720_hf4tn9.jpg", description: "In case you like a little more chill.")
-Kyle.create(name: "Wise Kyle", image_url: "http://res.cloudinary.com/duor0bzmo/image/upload/v1485032575/imageedit_3_2423010833_720_hf4tn9.jpg", description: "You look into his eyes and suddenly the world makes sense.")
+Kyle.create(name: "Wise Kyle", image_url: "http://res.cloudinary.com/duor0bzmo/image/upload/v1485032693/img_0330_720_nrdfyq.jpg", description: "You look into his eyes and suddenly the world makes sense.")
 Kyle.create(name: "Party Kyle", image_url: "http://res.cloudinary.com/duor0bzmo/image/upload/v1485032579/image_uploaded_from_ios_720_howrai.jpg", description: "Sometimes you just feel like letting loose.")
-Kyle.create(name: "Modest Kyle", image_url: "http://res.cloudinary.com/duor0bzmo/image/upload/v1485032689/img_0372_480_vfcmbh.jpg", description: "This Kyle does not actually exist.")
+Kyle.create(name: "Princess Kyle", image_url: "http://res.cloudinary.com/duor0bzmo/image/upload/v1485032689/img_0372_480_vfcmbh.jpg", description: "This Kyle does not actually exist.")
