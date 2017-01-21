34776e9625896690e2c52c82227f32a15f9db9c1
Amanda Beiner <aebeiner@gmail.com>
2017-01-21 16:26:59 -0500

    added header bar to application

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
-  has_many :matches
-  has_many :kyles, through: :users
 
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["info"]["name"]
+      user.image = auth["info"]["image"]
     end
   end
 end
