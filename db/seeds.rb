da1dfb63c80d4b1b3a2209d67adfaead4ec93b2a
Mike Thomson <michaelgt04@gmail.com>
2017-01-21 16:13:41 -0500

    Updated sign-in redirect and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Kyle.create(name: "Sport Kyle", image_url: "https://media.giphy.com/media/XZn9yRAjnVEQ0/giphy.gif", description: "For those who like the outdoors")
-Kyle.create(name: "Mediation Kyle", image_url: "http://i.imgur.com/nJvE8xd.gif", description: "In case you like a little more chill.")
+Kyle.create(name: "Sporty Kyle", image_url: "http://res.cloudinary.com/duor0bzmo/image/upload/v1485032696/img_0492_720_qfectr.jpg", description: "You like sports and so does he.")
+Kyle.create(name: "Mediation Kyle", image_url: "http://res.cloudinary.com/duor0bzmo/image/upload/v1485032575/imageedit_3_2423010833_720_hf4tn9.jpg", description: "In case you like a little more chill.")
+Kyle.create(name: "Wise Kyle", image_url: "http://res.cloudinary.com/duor0bzmo/image/upload/v1485032575/imageedit_3_2423010833_720_hf4tn9.jpg", description: "You look into his eyes and suddenly the world makes sense.")
+Kyle.create(name: "Party Kyle", image_url: "http://res.cloudinary.com/duor0bzmo/image/upload/v1485032579/image_uploaded_from_ios_720_howrai.jpg", description: "Sometimes you just feel like letting loose.")
+Kyle.create(name: "Modest Kyle", image_url: "http://res.cloudinary.com/duor0bzmo/image/upload/v1485032689/img_0372_480_vfcmbh.jpg", description: "This Kyle does not actually exist.")
