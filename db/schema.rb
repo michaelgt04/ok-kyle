3db614dcac347ba1ef320991673bbd951b8bc977
michaelgt04 <michaelgt04@gmail.com>
2017-01-21 09:21:24 -0500

    Added Kyle user model to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116223322) do
+ActiveRecord::Schema.define(version: 20170116234553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "kyles", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "image_url",  null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",        null: false
+    t.string   "image_url",   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
 end
