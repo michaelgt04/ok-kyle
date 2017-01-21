37f30ad2ed2bd65d5c7c67d2adef3961f8d65dee
Mike Thomson <michaelgt04@gmail.com>
2017-01-21 14:38:26 -0500

    Added swipe right functionality with API endpoint

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::MatchesController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def index
-    
+    binding.pry
+  end
+
+  def create
+    data = JSON.parse(request.body.read)
+    kyle = Kyle.find(data["id"])
+    Match.create(user: current_user, kyle: kyle)
   end
 end
