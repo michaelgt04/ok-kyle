811d0cd8478acba1a02af616991096693bee1b88
Mike Thomson <michaelgt04@gmail.com>
2017-02-14 11:24:24 -0500

    Added Kyle functionality (accommodate for admin model)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,20 @@ class MessagesController < ApplicationController
 
   def create
     message = Message.new(message_params)
+    if current_user.admin?
+      send_kyle_message(message)
+    else
+      send_user_message(message)
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :chatroom_id)
+  end
+
+  def send_user_message(message)
     message.user = current_user
     if message.save
       ActionCable.server.broadcast 'messages',
@@ -13,10 +27,16 @@ class MessagesController < ApplicationController
     end
   end
 
-  private
-
-  def message_params
-    params.require(:message).permit(:content, :chatroom_id)
+  def send_kyle_message(message)
+    message.admin = current_user
+    if message.save
+      ActionCable.server.broadcast 'messages',
+        message: message.content,
+        user: message.admin.name
+      head :ok
+    else
+      redirect_to chatrooms_path
+    end
   end
 
 end
