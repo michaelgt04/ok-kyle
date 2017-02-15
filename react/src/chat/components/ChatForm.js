f4a662e885945aaf3559737050350e73fee47fda
Mike Thomson <michaelgt04@gmail.com>
2017-02-14 21:01:29 -0500

    Added message label to React form for message submit

--- react/src/chat/components/ChatForm.js
+++ react/src/chat/components/ChatForm.js
@@ -3,6 +3,7 @@ import React from 'react';
 const ChatForm = props => {
   return(
     <form>
+      <label>Message: </label>
       <input type="text" onChange={props.handleMessageChange} value={props.value}/>
 
       <input type="submit" onClick={props.handleSubmit}/>
