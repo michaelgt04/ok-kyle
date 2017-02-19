4e1244ab00d22643031b23755af147e1e32a3fa2
Amanda Beiner <aebeiner@gmail.com>
2017-02-19 18:34:44 -0500

    fix auth permissions issues for signed in users and admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
 
   def index
+    authenticate_user!
   end
 
   def new
+    if signed_in_user
+      redirect_to users_path
+    elsif signed_in_admin
+      redirect_to admins_path
+    end
   end
 
   def show
   end
-  
+
 end
